30f910fd0c25373d91276459410d42d711040eb2
Jian Wei <jianwei@jianwei-deb8-64>
2016-10-21 17:47:32 -0400

    first edit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+class solution {
+  public:
+    std::string originalDigits(std::string & s) {
+        std::multiset<char> dict(s.begin(), s.end());
+    }
+};
+
+int main() {
+    
+}
